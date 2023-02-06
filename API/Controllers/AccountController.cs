ed867d3138b89b9d406c7032fd83b583afd46595
Vimal1020 <vimalmessi107@gmail.com>
2023-02-07 00:20:35 +0530

    end of section5

--- API/Controllers/AccountController.cs
+++ API/Controllers/AccountController.cs
@@ -51,7 +51,7 @@ namespace API.Controllers
         [HttpPost("login")]
         public async Task<ActionResult<UserDto>> Login(LoginDto loginDto)
         {
-                var user = await _context.Users.SingleOrDefaultAsync(x => x.UserName == loginDto.Username);
+                var user = await _context.Users.SingleOrDefaultAsync(x => x.UserName == loginDto.Username.Trim());
 
                 if (user == null) return Unauthorized("Invalid username");
 
