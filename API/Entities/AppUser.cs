0df920bc32d47c74c554106f6d670308dc83fb40
Vimal1020 <vimalmessi107@gmail.com>
2023-02-13 14:19:38 +0530

    data new

--- API/Entities/AppUser.cs
+++ API/Entities/AppUser.cs
@@ -2,11 +2,12 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using API.Extensions;
 
 namespace API.Entities
 {
     public class AppUser
-    {
+    { 
         public int Id { get; set; }
 
         public string UserName { get; set; }
@@ -36,6 +37,11 @@ namespace API.Entities
         public string Country { get; set; }
 
         public ICollection<Photo> Photos { get; set; }
+
+        public int GetAge()
+        {
+            return DateOfBirth.CalculateAge(); 
+        }
     }
 
 }
