ed1a835dcbbc5dce7bdc8ffcbecf38e09da3dcc3
Vimal1020 <vimalmessi107@gmail.com>
2023-09-03 13:59:18 +0530

    commit end of section 10

--- client/src/app/app-routing.module.ts
+++ client/src/app/app-routing.module.ts
@@ -6,8 +6,8 @@ import { TestErrorsComponent } from './errors/test-errors/test-errors.component'
 import { HomeComponent } from './home/home.component';
 import { ListsComponent } from './lists/lists.component';
 import { MemberDetailsComponent } from './members/member-details/member-details.component';
+import { MemberEditComponent } from './members/member-edit/member-edit.component';
 import { MemberListComponent } from './members/member-list/member-list.component';
-import { MembersListComponent } from './members/members-list/members-list.component';
 import { MessagesComponent } from './messages/messages.component';
 import { AuthGuard } from './_guards/auth.guard';
 
@@ -18,6 +18,7 @@ const routes: Routes = [
   children: [
     {path: 'members', component: MemberListComponent},
     {path: 'members/:username', component: MemberDetailsComponent},
+    {path: 'member/edit', component: MemberEditComponent},
     {path: 'lists', component: ListsComponent},
     {path: 'messages', component: MessagesComponent},
   ]
