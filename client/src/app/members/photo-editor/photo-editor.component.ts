df25a15ceaa28a461b5ee6285fa0cd4065e975e6
Vimal1020 <vimalmessi107@gmail.com>
2023-09-04 12:59:56 +0530

    commit

---
+++ client/src/app/members/photo-editor/photo-editor.component.ts
@@ -0,0 +1,59 @@
+import { AUTO_STYLE } from '@angular/animations';
+import { Component, Input } from '@angular/core';
+import { FileUploader } from 'ng2-file-upload/file-upload/file-uploader.class';
+import { Member } from 'src/app/_models/Member';
+import { User } from 'src/app/_models/user';
+import { AccountService } from 'src/app/_services/account.service';
+import { environment } from 'src/environments/environment';
+import { take } from 'rxjs/operators';
+
+@Component({
+  selector: 'app-photo-editor',
+  templateUrl: './photo-editor.component.html',
+  styleUrls: ['./photo-editor.component.css']
+})
+export class PhotoEditorComponent {
+  @Input() member: Member;
+  uploader: FileUploader;
+  hasBaseDropzoneOver = false;
+  baseUrl = environment.apiUrl;
+  user:User;
+
+  constructor(private accountService: AccountService){
+   this.accountService.currentUser$.pipe(take(1)).subscribe(user => this.user = user) 
+  }
+
+  ngOnInit(){
+    this.initializeUploader();
+  }
+
+  fileOverBase(e:any){
+    this.hasBaseDropzoneOver = e;
+  }
+
+  initializeUploader()
+  {
+    this.uploader = new FileUploader({
+      url: this.baseUrl+'users/add-photo',
+      authToken: 'Bearer '+ this.user.token,
+      isHTML5: true,
+      allowedFileType: ['image'],
+      removeAfterUpload: true,
+      autoUpload: false,
+      maxFileSize: 10 * 1024 * 1024
+    });
+
+    this.uploader.onAfterAddingFile = (file) => {
+      file.withCredentials = false;
+    }
+
+    this.uploader.onSuccessItem = (item, response, status, header) => 
+    {
+      if(response)
+      {
+        const photo = JSON.parse(response);
+        this.member.photos.push(photo);
+      }
+    }
+  }
+}
