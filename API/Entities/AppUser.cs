b6bee9baf3c9ee722495941aaadd99bc4a2dbbbf
Vimal1020 <vimalmessi107@gmail.com>
2023-09-08 21:49:07 +0530

    end of section 14

--- API/Entities/AppUser.cs
+++ API/Entities/AppUser.cs
@@ -38,6 +38,9 @@ namespace API.Entities
 
         public ICollection<Photo> Photos { get; set; }
 
+        public ICollection<UserLike> LikedByUsers { get; set; }
+        public ICollection<UserLike> LikedUsers { get; set; }
+
         // public int GetAge()
         // {
         //     return DateOfBirth.CalculateAge(); 
